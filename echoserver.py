44127d31f3c0b49298a518a623fbacb7ff3b4689
Joseph Miguel <ultrarelativistic@gmail.com>
2016-12-06 16:16:45 -0800

    init

--- echoserver.py
+++ echoserver.py
@@ -12,53 +12,58 @@ PAT = 'EAAKlBTVgof8BAOmqh2lLJoRbnZAbO5uG2p0xe5MR8XjrOtDyogMxMabAs5XZCrthaqfpLeA1
 
 @app.route('/', methods=['GET'])
 def handle_verification():
-  print("Handling Verification.")
-  if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
-    print("Verification successful!")
-    return request.args.get('hub.challenge', '')
-  else:
-    print("Verification failed!")
-    return "Error, wrong validation token"
+    print("Handling Verification.")
+    if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
+        print("Verification successful!")
+        return request.args.get('hub.challenge', '')
+    else:
+        print("Verification failed!")
+        return "Error, wrong validation token"
 
 
 @app.route('/', methods=['POST'])
 def handle_messages():
-  print("Handling Messages")
-  payload = request.get_data()
-  print(payload)
-  for sender, message in messaging_events(payload):
-    print("Incoming from %s: %s" % (sender, message))
-    send_message(PAT, sender, message)
-  sys.stdout.flush()
-  return "ok"
+    try:
+        print("Handling Messages")
+        payload = request.get_data()
+        print(payload)
+        for sender, message in messaging_events(payload):
+            print("Incoming from %s: %s" % (sender, message))
+            send_message(PAT, sender, message)
+        sys.stdout.flush()
+        return "ok"
+    finally:
+        print('Completed')
+        sys.stdout.flush()
+
 
 def messaging_events(payload):
-  """Generate tuples of (sender_id, message_text) from the
-  provided payload.
-  """
-  data = json.loads(payload)
-  messaging_events = data["entry"][0]["messaging"]
-  for event in messaging_events:
-    if "message" in event and "text" in event["message"]:
-      yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
-  else:
-    yield event["sender"]["id"], "I can't echo this"
+    """Generate tuples of (sender_id, message_text) from the
+    provided payload.
+    """
+    data = json.loads(payload)
+    messaging_events = data["entry"][0]["messaging"]
+    for event in messaging_events:
+        if "message" in event and "text" in event["message"]:
+            yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
+        else:
+            yield event["sender"]["id"], "I can't echo this"
 
 
 def send_message(token, recipient, text):
-  """Send the message text to recipient with id recipient.
-  """
+    """Send the message text to recipient with id recipient.
+    """
 
-  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
     params={"access_token": token},
     data=json.dumps({
-      "recipient": {"id": recipient},
-      "message": {"text": text.decode('unicode_escape')}
+        "recipient": {"id": recipient},
+        "message": {"text": text.decode('unicode_escape')}
     }),
     headers={'Content-type': 'application/json'})
-  if r.status_code != requests.codes.ok:
-    print(r.text)
+    if r.status_code != requests.codes.ok:
+        print(r.text)
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
 

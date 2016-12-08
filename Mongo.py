484af3dfed0434ea8232b65c61698dc5e65e652d
Joseph Miguel <ultrarelativistic@gmail.com>
2016-12-08 10:28:27 -0800

    x

--- Mongo.py
+++ Mongo.py
@@ -3,19 +3,19 @@ import os
 
 class DB:
 
-    _client = MongoClient(os.environ['MONGODB_URI'])
-    _db = _client['mrrogers']
-    _incoming = _db['incoming']
-    _outgoing = _db['outgoing']
+    # _client = MongoClient(os.environ['MONGODB_URI'])
+    # _db = _client['mrrogers']
+    # _incoming = _db['incoming']
+    # _outgoing = _db['outgoing']
 
     def does_user_exist(self):
         return True
 
     def record_incoming_message(self, payload):
-        self._incoming.insert_one(payload)
+        #self._incoming.insert_one(payload)
         return True
 
     def record_outgoing_message(self, payload):
-        self._outgoing.insert_one(payload)
+        #self._outgoing.insert_one(payload)
         return True
 

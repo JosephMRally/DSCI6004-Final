98ffca5d0aaf6771c7a4cb888f9fc1d22fb9dbf8
Joseph Miguel <ultrarelativistic@gmail.com>
2016-12-19 18:24:30 -0800

    attempting to fix video messages

--- echoserver.py
+++ echoserver.py
@@ -109,7 +109,7 @@ def send_message(token, recipient, text):
         data['message'] = {
                 "attachment": {
                     "type": "video",
-                    "payload": {"url": "https://youtu.be/XwvjMg_O_Wg"}
+                    "payload": {"url": "https://youtube.com/embed/VyLgiPItJj0"}
                 }
             }
 
